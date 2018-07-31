13635e883df63ba24c94437582101219a6c577f9
Meowchelle <txf0990@gmail.com>
2018-07-31 17:50:41 +1000

    V1.3:different users, users' own fridge/receipes

--- classes.py
+++ classes.py
@@ -10,11 +10,13 @@ class Database(object):
         self.c.execute("CREATE TABLE IF NOT EXISTS receipes (dish_id integer PRIMARY KEY AUTOINCREMENT, dish_name text, components text)")
         self.c.execute("CREATE TABLE IF NOT EXISTS materials (material_id integer PRIMARY KEY AUTOINCREMENT, material_name text)")
         self.c.execute("CREATE TABLE IF NOT EXISTS fridges (user_id integer PRIMARY KEY AUTOINCREMENT, stock text)")
+        self.c.execute("CREATE TABLE IF NOT EXISTS users (user_id integer PRIMARY KEY AUTOINCREMENT, user_name text UNIQUE, password text)")
+        self.c.execute("CREATE TABLE IF NOT EXISTS user_receipes (user_id integer, user_receipes text)")
         self.conn.commit()
 
-    def insertUser(self):
+    '''def insertUser(self):
         self.c.execute("INSERT INTO fridges (stock) Values(?)", ('',))
-        self.conn.commit()
+        self.conn.commit()'''
 
     def insertReceipeEntry(self, dish_name, materials):
         materials_text = ""
@@ -23,11 +25,58 @@ class Database(object):
             materials_text = materials_text + item + " "
         materials_text = materials_text[:-1]
         self.c.execute("INSERT INTO receipes (dish_name, components) Values(?,?)", (UTF8_to_Unicode(dish_name), UTF8_to_Unicode(materials_text)))
+        cursor_object = self.c.execute("SELECT * FROM receipes WHERE dish_id=last_insert_rowid()");
+        list_cursor_object = list(cursor_object)
         self.conn.commit()
+        return list_cursor_object[0][0] # return a int.
             
     def deleteReceipeEntry(self, dish_id):
         self.c.execute("DELETE FROM receipes WHERE dish_id=?",(dish_id,))
         self.conn.commit()
+
+    def deliverReceipe(self):
+        cursor_object = self.c.execute("SELECT * FROM receipes")
+        list_cursor_object = list(cursor_object)    # list_cursor_object is a list of tuple, like: [(1, u'HongShaoRou', u'ZhuRou Tang BaJiao'), (3, u'XiHongShiChaoJiDan', u'XiHongShi JiDan Tang'), (4, u'TangCuPaiGu', u'PaiGu Tang Cu')]
+        #list_cursor_object = map(lambda (a, b, c):(a, Unicode_to_UTF8(b), Unicode_to_UTF8(c)),
+        #        list_cursor_object)
+        return list_cursor_object
+########
+    #  联合查询
+    def insertMyReceipeEntry(self, user_id, dish_name, materials):
+        last_insert_id = self.insertReceipeEntry(dish_name, materials)
+        cursor_object = self.c.execute("SELECT * FROM user_receipes WHERE user_id=?",(user_id,))
+        list_cursor_object = list(cursor_object)        # [(1,'1 2 3')]
+        receipe_list_text = list_cursor_object[0][1]
+        receipe_list = receipe_list_text.split(' ')     # ['1','2','3']
+        if not str(last_insert_id) in receipe_list:
+            receipe_list_text = receipe_list_text + ' ' + str(last_insert_id)
+            self.c.execute("UPDATE user_receipes SET user_receipes=? WHERE user_id=?", (receipe_list_text, user_id))    
+            self.conn.commit()
+    
+    def deleteMyReceipeEntry(self, user_id, dish_id):
+        cursor_object = self.c.execute("SELECT * FROM user_receipes WHERE user_id=?",(user_id,))
+        list_cursor_object = list(cursor_object)        # [(1,'1 2 3')]
+        receipe_list = list_cursor_object[0][1].split(' ')  #['1','2','3']
+        receipe_list.remove(str(dish_id))
+        receipe_list_text = ' '.join(receipe_list)
+        self.c.execute("UPDATE user_receipes SET user_receipes=? WHERE user_id=?", (receipe_list_text, user_id))
+        self.conn.commit()
+
+    def deliverMyReceipe(self, user_id):
+        cursor_object = self.c.execute("SELECT * FROM user_receipes WHERE user_id=?", (user_id,))
+        list_cursor_object = list(cursor_object)    # a list of receipes to show: [(1,'1 2 3')] user_id, user_receipes
+        list_cursor_object = list_cursor_object[0][1].split(' ')        # ['1','2','3']
+        receipes_to_show = []
+        for dish_id in list_cursor_object:
+            temp_object = self.c.execute("SELECT * FROM receipes WHERE dish_id=?", (int(dish_id),))
+            list_temp_object = list(temp_object)        # [(1,'西红柿炒鸡蛋','西红柿 鸡蛋')]
+            if len(list_temp_object) != 0:
+                temp_tuple = list_temp_object[0]
+                receipes_to_show.append(temp_tuple)
+        return receipes_to_show               # I want to return a list of tuple.
+
+#######
+
 
     def insertToFridge(self, user_id, stock):  # stock must not exist in fridge
         cursor_object = self.c.execute("SELECT stock FROM fridges WHERE user_id=?", (user_id,))
@@ -58,19 +107,21 @@ class Database(object):
         self.c.execute("UPDATE fridges SET stock=? WHERE user_id=?", (stock_text, user_id))
         self.conn.commit()
     
-    def deliverReceipe(self):
-        cursor_object = self.c.execute("SELECT * FROM receipes")
-        list_cursor_object = list(cursor_object)    # list_cursor_object is a list of tuple, like: [(1, u'HongShaoRou', u'ZhuRou Tang BaJiao'), (3, u'XiHongShiChaoJiDan', u'XiHongShi JiDan Tang'), (4, u'TangCuPaiGu', u'PaiGu Tang Cu')]
-        #list_cursor_object = map(lambda (a, b, c):(a, Unicode_to_UTF8(b), Unicode_to_UTF8(c)),
-        #        list_cursor_object)
-        return list_cursor_object
-    
     def deliverFridge(self, user_id):
         cursor_object = self.c.execute("SELECT * FROM fridges WHERE user_id=?", (user_id,))
         list_cursor_object = list(cursor_object)    # list_cursor_object is a list of tuple
         #list_cursor_object = map(lambda (a, b):(a, Unicode_to_UTF8(b)),
         #        list_cursor_object)
         return list_cursor_object
+
+    def verifyPassword(self, user_name, password):
+        cursor_object = self.c.execute("SELECT * FROM users WHERE user_name=?", (user_name,))
+        list_cursor_object = list(cursor_object)
+        if len(list_cursor_object) == 0:
+            return -1
+        if (list_cursor_object[0][2] != password):
+            return -1
+        return list_cursor_object[0][0]
 
 def UTF8_to_Unicode(s):
     return s.decode('utf8')
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, request, redirect, url_for
+# -*- coding: UTF-8 -*-
+from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 from classes import Database
 
@@ -13,12 +14,36 @@ def Unicode_to_UTF8(s):
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'This is the home page'
+def index():
+    if 'user_id' in session:
+        return redirect(url_for('debug'))
+    return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_name = request.form['user_name']
+        password = request.form['password']
+        database = Database(filename)
+        user_id = database.verifyPassword(user_name, password)
+        if user_id != -1:                # 判断用户名密码是否匹配
+            session['user_id'] = user_id
+            return redirect(url_for('result'))
+    return redirect(url_for('index'))
+    
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
+@app.route('/debug')
+def debug():
+    user_id = session['user_id']
+    return render_template('debug.html', user_id=user_id)
 
 @app.route('/hello')
 def welcome():
-    return 'Hello!'
+    return 'Hello, %s' % session['user_id']
 
 @app.route('/hello/<username>')
 def hello_sb(username):
@@ -26,9 +51,20 @@ def hello_sb(username):
 
 @app.route('/receipes')
 def receipes():
-    database = Database(filename)
-    receipes = database.deliverReceipe()
-    return render_template('receipes.html', receipes=receipes)
+    if 'user_id' in session:
+        database = Database(filename)
+        receipes = database.deliverReceipe()
+        return render_template('receipes.html', receipes=receipes)
+    return render_template('login.html')
+
+@app.route('/myreceipes')
+def my_receipes():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        database = Database(filename)
+        receipes = database.deliverMyReceipe(user_id)
+        return render_template('my_receipes.html', receipes=receipes)
+    return render_template('login.html')
 
 @app.route('/receipes/insert', methods=['GET', 'POST'])
 def receipes_insert():
@@ -45,25 +81,53 @@ def receipes_insert():
         database.insertReceipeEntry(dish_name, components)
     return redirect(url_for('receipes'))
 
+@app.route('/myreceipes/insert', methods=['GET', 'POST'])
+def my_receipes_insert():
+    database = Database(filename)
+    user_id = session['user_id']
+    if request.method == 'POST':
+        content = request.form['content']
+        if content == '':
+            return redirect(url_for('my_receipes'))
+        content = Unicode_to_UTF8(content)
+        content = content.split(' ')
+        dish_name = content[0]
+        content.remove(dish_name)
+        components = content
+        database.insertMyReceipeEntry(user_id, dish_name, components)
+    return redirect(url_for('my_receipes'))
+
 @app.route('/receipes/delete', methods=['GET', 'POST'])
 def receipes_delete():
     database = Database(filename)
     database.deleteReceipeEntry(request.args['id'])
     return redirect(url_for('receipes'))
 
+@app.route('/myreceipes/delete', methods=['GET', 'POST'])
+def my_receipes_delete():
+    user_id = session['user_id']
+    database = Database(filename)
+    database.deleteMyReceipeEntry(user_id, request.args['id'])
+    return redirect(url_for('my_receipes'))
+
 @app.route('/fridges')
 def fridges():
-    database = Database(filename)
-    user_id = 1
-    fridges = database.deliverFridge(user_id)
-    fridges = fridges[0][1].split(' ')
-    # now fridges is a list of stock
-    return render_template('fridges.html', fridges=fridges)
+    if 'user_id' in session:
+        database = Database(filename)
+        user_id = session['user_id']
+        #user_id = 1
+        fridges = database.deliverFridge(user_id)
+        if len(fridges) != 0:
+            fridges = fridges[0][1].split(' ')
+        # now fridges is a list of stock
+        return render_template('fridges.html', fridges=fridges)
+    return render_template('login.html')
 
 @app.route('/fridges/insert', methods=['GET','POST'])
 def fridges_insert():
     database = Database(filename)
-    user_id = 1
+    user_id = session['user_id']
+    #user_id = 1
     # user_fridge is a list of what is already in fridge
     if request.method == 'POST':
         add_stock_text = request.form['content']
@@ -76,56 +140,61 @@ def fridges_insert():
 @app.route('/fridges/delete', methods=['GET','POST'])
 def fridges_delete():
     database = Database(filename)
-    user_id = 1
+    user_id = session['user_id']
+    #user_id = 1
     material = request.args['name']
-    print 'calling deleteFromFridge({},{})'.format(user_id, [material])
+    #print 'calling deleteFromFridge({},{})'.format(user_id, [material])
     database.deleteFromFridge(user_id, [material])
     return redirect(url_for('fridges'))
 
 @app.route('/result', methods=['GET','POST'])
 def result():
-    database = Database(filename)
-    user_id = 1
-    receipes = database.deliverReceipe()
-    fridge = database.deliverFridge(user_id)
-    fridge = fridge[0][1]
-    fridge = fridge.split(' ')
-    # fridge is a list of what you have-unicode
-    # receipes is a list of tuple 
-    receipe_list = list(receipes)
-    result_list = []
-    for one_receipe in receipes:
-        dish_num = one_receipe[0]
-        dish_name = one_receipe[1]
-        still_need = one_receipe[2].split(' ') #components is a list of materials.
-        what_i_have = []
-        for item in fridge:
-            if item in still_need:
-                still_need.remove(item)
-                what_i_have.append(item)
-        this_entry = []
-        this_entry.append(dish_num)
-        this_entry.append(dish_name)
-        this_entry.append(what_i_have)
-        this_entry.append(still_need)
-        this_entry.append(len(still_need))
-        result_list.append(this_entry)
+    if 'user_id' in session:
+        database = Database(filename)
+        user_id = session['user_id']
+        #user_id = 1
+        receipes = database.deliverMyReceipe(user_id)
+        fridge = database.deliverFridge(user_id)
+        if len(fridge) != 0:
+            fridge = fridge[0][1]
+            fridge = fridge.split(' ')
+        # fridge is a list of what you have-unicode
+        # receipes is a list of tuple 
+        receipe_list = list(receipes)
+        result_list = []
+        for one_receipe in receipes:
+            dish_num = one_receipe[0]
+            dish_name = one_receipe[1]
+            still_need = one_receipe[2].split(' ') #components is a list of materials.
+            what_i_have = []
+            for item in fridge:
+                if item in still_need:
+                    still_need.remove(item)
+                    what_i_have.append(item)
+            this_entry = []
+            this_entry.append(dish_num)
+            this_entry.append(dish_name)
+            this_entry.append(what_i_have)
+            this_entry.append(still_need)
+            this_entry.append(len(still_need))
+            result_list.append(this_entry)
 
-    sorted_result = []
-    while len(result_list) != 0:
-        min = 10000
-        to_delete = 0
-        for line in result_list:
-            if line[4] < min:
-                min = line[4]
-                to_delete = line
-        sorted_result.append(to_delete)
-        result_list.remove(to_delete)
-    # soted_result is a list:
-    # [dish_num, dish_name, list(what_i_have), list(material still needed), item number still needed]
-    return render_template('result.html', data=sorted_result)
+        sorted_result = []
+        while len(result_list) != 0:
+            min = 10000
+            to_delete = 0
+            for line in result_list:
+                if line[4] < min:
+                    min = line[4]
+                    to_delete = line
+            sorted_result.append(to_delete)
+            result_list.remove(to_delete)
+        # soted_result is a list:
+        # [dish_num, dish_name, list(what_i_have), list(material still needed), item number still needed]
+        return render_template('result.html', data=sorted_result)
+    return render_template('login.html')
 
-
+app.secret_key = '\ng\xaa!\x01\xd8\xd2%Ftz}m\xf0\xa1\xe6\xdf\xe8I\x8a\xb8\x80\xb6\x90'
 
 
 if __name__ == '__main__':
